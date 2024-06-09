898939645048147a650c54c88829089d85390487
beothorn <lucass@gmail.com>
2024-06-09 22:10:15 +0200

    Loader experiment start

---
+++ client/src/main/java/br/com/isageek/App.java
@@ -0,0 +1,13 @@
+package br.com.isageek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
