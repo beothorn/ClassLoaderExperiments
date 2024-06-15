897ea0d0399376b29ac4fde18b0aacc50f19a6f1
beothorn <lucass@gmail.com>
2024-06-16 01:04:54 +0200

    Testing props injection

--- client/src/main/java/br/com/isageek/App.java
+++ client/src/main/java/br/com/isageek/App.java
@@ -7,6 +7,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("[Client] Hello World!");
+        for (final String arg : args) {
+            System.out.println("[Client] arg: "+arg);
+        }
+        String clientEnv = System.getenv("clientEnv");
+        System.out.println("[Client] clientEnv: "+clientEnv);
+        String clientProp = System.getProperty("clientProp");
+        System.out.println("[Client] clientProp: "+clientProp);
+        String fileSeparator = System.getProperty("file.separator");
+        System.out.println("[Client] fileSeparator: "+fileSeparator);
+
         callServer(StringUtils.upperCase("Some data"));
         SomeOtherClass someOtherClass = new SomeOtherClass();
         System.out.println(someOtherClass.getPayload("foo"));
